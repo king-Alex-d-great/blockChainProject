e7db695274dfbceb0b13440068c2a3aa11d962af
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-17 08:24:13 +0100

    Updated Import to come from github

--- contracts/KingsNFT.sol
+++ contracts/KingsNFT.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "./Owner.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
 contract KingNFT is ERC721Full, Owner{
     uint [] public KingNFTTokens;
@@ -18,44 +18,36 @@ contract KingNFT is ERC721Full, Owner{
     constructor() ERC721Full("KingNFT", "KNFT"){
     }
 
-    function mint() external onlyOwner{
+    function mint() external isOwner{
     //generate a random number
     uint _tokenId;
-    require (!_TokenIdExists[_tokenId]);
-    uint _id = KingNFTTokens.push(tokenId);
-    _mint(msg.sender, _id);
+    require (!_TokenIdExists[_tokenId]);     
+    _mint(msg.sender, _tokenId);
     OwnerTokenCount[msg.sender]++; 
-    _randomStringExists[tokenId] = true;
+    _TokenIdExists[_tokenId]  = true;
     }
 
-    function balanceOf(address _owner) external view returns (uint256) {
+    function balanceOfAddress(address _owner) external override view returns (uint256) {
     // 1. Return the number of tokens the `_owner` has here
     return OwnerTokenCount[_owner];
     }
 
-  function ownerOf(uint256 _tokenId) external view returns (address) {
+  function ownerOf(uint256 _tokenId) external override view returns (address) {
     // 2. Return the owner of `_tokenId` here
     return TokenToOwner[_tokenId];
   }
 
-  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+  function transferFrom(address _from, address _to, uint256 _tokenId) public override {
       //require approved owner to do the calling
       require (TokenApproval[_tokenId] == msg.sender);
         _transfer(_from, _to, _tokenId);
-  }
+  }  
 
-  function _transfer (address _from, address _to, uint256 _tokenId) private {
-    TokenToOwner[_tokenId] = _to;
-    OwnerTokenCount[_from]--;
-    OwnerTokenCount[_to]++;
-    emit Transfer(_from, _to, _tokenId);
-  }
-
-  function approve(address _approved, uint256 _tokenId) external payable {
+  function approve(address _approved, uint256 _tokenId) public override {
       //require sender actually owns _tokenId
     require (TokenToOwner[_tokenId] == msg.sender);
       //Add approve to the _approved address
-    TokenApproval[_tokenId] = approved; 
+    TokenApproval[_tokenId] = _approved; 
     emit Approval(msg.sender, _approved, _tokenId);     
   }
 } 
